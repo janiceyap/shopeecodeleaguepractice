5ed8ccd88d3811c3ea63ee9c7a212f196d82b413
janiceyap <janice.sl.yap@gmail.com>
2022-03-01 11:23:16 +0800

    still need more work. :)

--- 3OrderDelivery/handleInput.js
+++ 3OrderDelivery/handleInput.js
@@ -1,16 +1,13 @@
 function handleInput(input){
+    let repeatOrderCity = -1;
     const inputObj = {
         N : input[0][0],
         D : input[0][1],
         E : input[0][2],
+        M : input[][0],
         adjCity : [],
-        W : [],
-        C : [],
-        P : [],
         warehouse : [],
-        M : null,
-        K : [],
-        G : []
+        order : [],
     };
 
     inputObj.M = input[inputObj.E + inputObj.D + 1];
@@ -31,19 +28,37 @@ function handleInput(input){
 
     for (let j=0; j<inputObj.D; j++){
 
-        // consolidate supply points by city
+        // all cities have at most 1 warehouse, means won't be repeated.
 
-        inputObj.W.push(input[1+inputObj.E+j][0]);
-        inputObj.C.push(input[1+inputObj.E+j][1]);
-        inputObj.P.push(input[1+inputObj.E+j][2]);
+        inputObj.warehouse.push({
+            qty : input[1+inputObj.E+j][0],
+            cost : input[1+inputObj.E+j][1],
+            city : input[1+inputObj.E+j][2]
+        });
+
+        // inputObj.W.push(input[1+inputObj.E+j][0]);
+        // inputObj.C.push(input[1+inputObj.E+j][1]);
+        // inputObj.P.push(input[1+inputObj.E+j][2]);
     };
 
     for (let k=0; k<inputObj.M; k++){
 
         // consolidate orders by city
+
+        repeatOrderCity = inputObj.order.findIndex(e => e.city === input[inputObj.E + inputObj.D + 2+k][1]);
+        console.log("repeatOrderCity", repeatOrderCity);
+
+        if ( repeatOrderCity === -1){
+            inputObj.order.push({
+                qty : input[inputObj.E + inputObj.D + 2+k][0],
+                city : input[inputObj.E + inputObj.D + 2+k][1]
+            });
+        } else {
+            inputObj.order[repeatOrderCity].qty = inputObj.order[repeatOrderCity].qty + input[inputObj.E + inputObj.D + 2+k][0];
+        }
         
-        inputObj.K.push(input[inputObj.E + inputObj.D + 2+k][0]);
-        inputObj.G.push(input[inputObj.E + inputObj.D + 2+k][1]);
+        // inputObj.K.push(input[inputObj.E + inputObj.D + 2+k][0]);
+        // inputObj.G.push(input[inputObj.E + inputObj.D + 2+k][1]);
     };
 
     return inputObj;
--- 3OrderDelivery/index.js
+++ 3OrderDelivery/index.js
@@ -3,12 +3,49 @@ const handleInput = require("./handleInput");
 function minDeliveryCost(input){
     let output = 0;
     let inputObj = handleInput(input);
-
+    let distanceMatrix = []; // an array of objects that hold arrays of cities 1km, 2km and 3km apart.
 
     // console.log(inputObj);
-    console.log(inputObj.adjCity);
+    console.log("adjCity", inputObj.adjCity);
+    console.log("warehouse", inputObj.warehouse);
+    console.log("order", inputObj.order);
 
     // objective: minimize cost while working within limited supply
+
+    // work out the distance matrix for each order.
+    for (let i=0; i<inputObj.order.length; i++){
+
+        one = [];
+        two = [];
+        three = [];
+        four = [];
+        five = [];
+
+        one = inputObj.adjCity.map();
+
+
+
+
+
+        distanceMatrix[i].push({
+            onekm : one, 
+            twokm : two,
+            threekm : three, 
+            fourkm : four,
+            fivekm : five
+        });
+
+
+
+
+
+
+
+
+
+
+
+    }
 
 
 
